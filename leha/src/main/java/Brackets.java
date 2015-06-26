ec648d5591311bb54248bbd01ac564b9adfe0f71
Oleksii <voleksiy@gmail.com>
2015-06-26 13:51:47 +0300

    Brackets done

---
+++ leha/src/main/java/Brackets.java
@@ -0,0 +1,18 @@
+/**
+ * Created by avasilenko on 26.06.2015.
+ */
+public class Brackets {
+
+    boolean isCorrectBrackets(String str){
+        if (str.length() == 0)
+            return true;
+
+        if (str.charAt(0) == '(' && str.indexOf(")") != -1)
+        {
+
+            return isCorrectBrackets(str.substring(1, str.indexOf(")")) + str.substring(str.indexOf(")")+1));
+        }
+        else return false;
+    }
+}
+
