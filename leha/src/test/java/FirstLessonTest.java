34f32be86274a61071bdc677f9cfe5ad78dd4341
Oleksii <voleksiy@gmail.com>
2015-06-26 17:38:43 +0300

    Test class created

---
+++ leha/src/test/java/FirstLessonTest.java
@@ -0,0 +1,38 @@
+import org.junit.*;
+
+/**
+ * Created by avasilenko on 26.06.2015.
+ */
+
+import org.junit.Test;
+
+public class FirstLessonTest {
+
+    @Test
+    public void testBrackets()
+    {
+        Brackets b = new Brackets();
+        final boolean isCorrect = true;
+        Assert.assertEquals(isCorrect, b.isCorrectBrackets("()()()"));
+    }
+
+    @Test
+    public void testTempAnalyze()
+    {
+        final double dCorrrect = 1;
+        double [] ts = {-1,-15,-17,-3,+5,+17, 1};
+
+        TempAnalyze t = new TempAnalyze();
+        Assert.assertEquals(dCorrrect, t.closestToZero(ts), 0);
+    }
+
+    @Test
+    public void testTempAvg()
+    {
+        final double dCorrrect = -1;
+        double [] ts = {-1,-15,-17,-3,+5,+17, 1};
+
+        TempAvg t = new TempAvg();
+        Assert.assertEquals(dCorrrect, t.closestToAvg(ts), 0);
+    }
+}
